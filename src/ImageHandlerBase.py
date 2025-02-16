d5203c6423783dca17e802c25277bcee4699632d
Karl <k4rl.kapoor@gmail.com>
2025-02-16 22:30:18 +1300

    updated green to pastel green

--- src/ImageHandlerBase.py
+++ src/ImageHandlerBase.py
@@ -2,7 +2,14 @@ from multiprocessing import Pool
 from datetime import datetime
 import os
 from settings.initDirs import init_all
-from settings.staticDicts import valid_formats_cfg, image_quality_cfg, standard_img_size_cfg, log_location
+from settings.staticDicts import (
+    valid_formats_cfg,
+    image_quality_cfg,
+    standard_img_size_cfg,
+    log_location,
+)
+
+
 class ImageHandlerBase:
     def __init__(self):
         init_all()  # init prerequisite directories that are gitignored
@@ -14,9 +21,9 @@ class ImageHandlerBase:
         self.curr_height = 0
         self.curr_canvas_size = (0, 0)
         self.now = datetime.now()
-        self.img_dir = os.listdir('./img')
+        self.img_dir = os.listdir("./img")
         self.log = log_location
-        # self.max_cores = multiprocessing.cpu_count() 
+        # self.max_cores = multiprocessing.cpu_count()
 
     def fetch_imgs(self):
         """
@@ -34,7 +41,7 @@ class ImageHandlerBase:
         """
         imgs = self.fetch_imgs()
         # Set to 4 to prevent DE lagging. Typically smaller quantities of pictures are loaded together.
-        p = Pool(4)  # default processes = cpu_count().    
+        p = Pool(4)  # default processes = cpu_count().
         p.map(self.work_handler, imgs)
 
     def work_handler(self, file):
@@ -45,13 +52,15 @@ class ImageHandlerBase:
         """
         print("Requires override")
         return
-    
+
     def append_ad_hoc_comment_to_log(self, comment, log_file) -> None:
-        with open(f'{log_file}', 'a') as log:
+        with open(f"{log_file}", "a") as log:
             log.write(comment)
         return
 
-    def append_processed_result_to_log(self, start_time: float, end_time: float, img_file, log_file: str):
+    def append_processed_result_to_log(
+        self, start_time: float, end_time: float, img_file, log_file: str
+    ):
         """Append a message to the log saying whether the img was successfully processed or not
 
         Args:
@@ -60,9 +69,10 @@ class ImageHandlerBase:
             img_file (_type_): each image file in the img generator
             log_file (str): the name of the log in ./config
         """
-        with open(f'{log_file}', 'a') as log:
+        with open(f"{log_file}", "a") as log:
             log.write(
-                f'{self.now.strftime("%d/%m/%Y %H:%M")}: {img_file} processed in {round(end_time - start_time, 2)}s\n')
+                f'{self.now.strftime("%d/%m/%Y %H:%M")}: {img_file} processed in {round(end_time - start_time, 2)}s\n'
+            )
 
     def colour_sub(self, image, colour: tuple):
         """Method to replace pixels over a certain opacity with that of another specified colour
@@ -92,8 +102,8 @@ class ImageHandlerBase:
         Args:
             image_to_archive (Image): Image to archive
         """
-        os.replace(f'img/{image_to_archive}', f'img/zArchive/{image_to_archive}')
+        os.replace(f"img/{image_to_archive}", f"img/zArchive/{image_to_archive}")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     pass
